1f28650b9f9a709ad856f1ba2709d2acc3be848c
Josh Taylor <jstaylor926@gmail.com>
2020-06-24 09:58:13 -0400

    iPad Video Fix

--- js/auditScript.js
+++ js/auditScript.js
@@ -1,58 +1,28 @@
-const player = document.querySelector('.player');
-const video = player.querySelector('.player-video');
-const progress = player.querySelector('.progress');
-const progressFilled = player.querySelector('.filled-progress');
-const toggle = player.querySelector('.toggle-play');
-const skippers = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player-slider');
 
-// Logic
-function togglePlay() {
-    const playState = video.paused ? 'play' : 'pause';
-    video[playState](); // Call play or paused method
+function resizeVideo (){
+    const video = document.querySelector("#birdVideo");
+    video.width = 0.676 * window.innerWidth;
+    video.height = 0.6463 * window.innerHeight;
+    video.style.top = (0.1945 * window.innerHeight) + "px";
+    video.style.left = (0.1607 * window.innerWidth) + "px";
+
 }
+resizeVideo();
+window.addEventListener('resize',resizeVideo);
 
-function updateButton() {
-    const togglePlayBtn = document.querySelector('.toggle-play');
 
-    if(this.paused) {
-        togglePlayBtn.innerHTML = `<svg class="" width="16" height="16" viewBox="0 0 16 16"><title>play</title><path d="M3 2l10 6-10 6z"></path></svg>`;
-    } else {
-        togglePlayBtn.innerHTML = `<svg width="16" height="16" viewBox="0 0 16 16"><title>pause</title><path d="M2 2h5v12H2zm7 0h5v12H9z"></path></svg>`;
-    }
+
+function resizeVideoMobile (){
+    const video = document.querySelector("#birdVideoMobile");
+    video.width = 1.3326 * container.innerWidth;
+    video.height = 0.6463 * container.innerHeight;
+    video.style.top = (0.0987 * container.innerHeight) + "px";
+    video.style.left = (0.1607 * container.innerWidth) + "px";
+
 }
+resizeVideoMobile();
+window.addEventListener('resize',resizeVideoMobile);
 
-function skip() {
-    video.currentTime += parseFloat(this.dataset.skip);
-}
 
-function rangeUpdate() {
-    video[this.name] = this.value;
-}
 
-function progressUpdate() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressFilled.style.flexBasis = `${percent}%`;
-}
 
-function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-    video.currentTime = scrubTime;
-}
-
-// Event listeners
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', progressUpdate);
-
-toggle.addEventListener('click', togglePlay);
-skippers.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', rangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', rangeUpdate));
-
-let mousedown = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);
